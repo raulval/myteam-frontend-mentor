3a72794f3dc5639ae38c57e9c3ebbcbd3f6714f9
Raul Val <raulval@outlook.com>
2023-02-01 15:41:34 -0300

    ✨ add header in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Header from "@/components/Header";
 import { GlobalStyle } from "@/styles/globals";
 import Theme from "@/styles/theme";
 import { ThemeProvider } from "styled-components";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
+      <Header />
     </ThemeProvider>
   );
 }
