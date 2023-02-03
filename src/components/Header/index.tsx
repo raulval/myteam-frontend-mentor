641a228a08076b9535347479fa8ec1760f657701
Raul Val <raulval@outlook.com>
2023-02-03 12:24:36 -0300

    ✨ add hamburger on header in mobile screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,24 @@ import logo from "@/assets/logo.svg";
 import { useState } from "react";
 import {
   ContactButton,
+  ContactButtonWrapper,
   Container,
+  Hamburger,
   Logo,
   Menu,
   MenuLinkAbout,
   MenuLinkHome,
   MenuLinkWrapper,
+  MobileMenu,
   Wrapper,
 } from "./styles";
 
 const Header = () => {
   const [isActive, setIsActive] = useState("home");
+  const [extendNavbar, setExtendNavbar] = useState(false);
 
   return (
-    <Container>
+    <Container extendNavbar={extendNavbar}>
       <Wrapper>
         <Logo src={logo} alt="MyTeam logo" />
         <Menu>
@@ -35,8 +39,37 @@ const Header = () => {
               about
             </MenuLinkAbout>
           </MenuLinkWrapper>
-          <ContactButton>contact us</ContactButton>
+          <ContactButtonWrapper>
+            <ContactButton>contact us</ContactButton>
+          </ContactButtonWrapper>
         </Menu>
+        <Hamburger
+          onClick={() => {
+            setExtendNavbar((curr) => !curr);
+          }}
+          extendNavbar={extendNavbar}
+        >
+          <div />
+          <div />
+          <div />
+        </Hamburger>
+        <MobileMenu extendNavbar={extendNavbar}>
+          <MenuLinkHome
+            href={"/"}
+            onClick={() => setIsActive("home")}
+            isactive={isActive}
+          >
+            home
+          </MenuLinkHome>
+          <MenuLinkAbout
+            href={"#"}
+            onClick={() => setIsActive("about")}
+            isactive={isActive}
+          >
+            about
+          </MenuLinkAbout>
+          <ContactButton>contact us</ContactButton>
+        </MobileMenu>
       </Wrapper>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,11 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
 
-interface MenuLinkProps {
+interface HeaderProps {
   isactive?: string;
+  extendNavbar?: boolean;
 }
 
-export const Container = styled.header`
+export const Container = styled.header<HeaderProps>`
   display: flex;
   width: 100vw;
   height: 5rem;
@@ -23,6 +24,10 @@ export const Wrapper = styled.nav`
   align-items: center;
 
   gap: 5rem;
+
+  @media (max-width: 768px) {
+    gap: 0;
+  }
 `;
 
 export const Logo = styled(Image)``;
@@ -37,9 +42,13 @@ export const Menu = styled.div`
 export const MenuLinkWrapper = styled.div`
   display: flex;
   gap: 2rem;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
-export const MenuLinkHome = styled(Link)<MenuLinkProps>`
+export const MenuLinkHome = styled(Link)<HeaderProps>`
   font-size: 1rem;
   font-weight: 600;
   color: ${(props) =>
@@ -59,7 +68,7 @@ export const MenuLinkHome = styled(Link)<MenuLinkProps>`
   }
 `;
 
-export const MenuLinkAbout = styled(Link)<MenuLinkProps>`
+export const MenuLinkAbout = styled(Link)<HeaderProps>`
   font-size: 1rem;
   font-weight: 600;
   color: ${(props) =>
@@ -76,6 +85,12 @@ export const MenuLinkAbout = styled(Link)<MenuLinkProps>`
 
   &:hover {
     color: ${(props) => props.theme.colors.primary.main};
+  }
+`;
+
+export const ContactButtonWrapper = styled.div`
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
 
@@ -96,5 +111,75 @@ export const ContactButton = styled.button`
   &:hover {
     background-color: #ffffff;
     color: ${(props) => props.theme.colors.secondary.main};
+  }
+`;
+
+export const Hamburger = styled.button<HeaderProps>`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 23px;
+  right: 30px;
+  z-index: 20;
+  width: 30px;
+  height: 30px;
+  background: none;
+  border: none;
+  color: ${(props) => props.theme.colors.primary.text};
+  font-size: 38px;
+  cursor: pointer;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background-color: ${(props) => props.theme.colors.primary.text};
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+
+    &:nth-child(1) {
+      transform: ${({ extendNavbar }) =>
+        extendNavbar ? "rotate(43deg)" : "rotate(0)"};
+    }
+    &:nth-child(2) {
+      transform: ${({ extendNavbar }) =>
+        extendNavbar ? "translateX(-100%)" : "translateX(0)"};
+      opacity: ${({ extendNavbar }) => (extendNavbar ? 0 : 1)};
+    }
+    &:nth-child(3) {
+      transform: ${({ extendNavbar }) =>
+        extendNavbar ? "rotate(-43deg)" : "rotate(0)"};
+    }
+  }
+`;
+
+export const MobileMenu = styled.div<HeaderProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.secondary.main};
+  position: fixed;
+  transform: ${({ extendNavbar }) =>
+    extendNavbar ? "translateX(0)" : "translateX(100%)"};
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 230px;
+  padding-top: 5rem;
+  gap: 2rem;
+  transition: transform 0.3s ease-in-out;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+
+  @media (min-width: 425px) {
+    width: 300px;
   }
 `;
