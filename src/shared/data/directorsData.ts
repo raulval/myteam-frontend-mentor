a59cf155eac0114519f33b83037006dd359ccdd0
Raul Val <raulval@outlook.com>
2023-02-07 14:44:45 -0300

    ✨ create directors data

---
+++ src/shared/data/directorsData.ts
@@ -0,0 +1,52 @@
+const directorsData = [
+  {
+    image: "/images/avatar-raul.png",
+    name: "Raul Val",
+    role: "Founder & CEO",
+    quote: `“It always amazes me how much talent there is in every corner of the globe.”`,
+    twitter: "https://twitter.com/raul_val",
+    linkedin: "https://www.linkedin.com/in/raulval/",
+  },
+  {
+    image: "/images/avatar-cristian.jpg",
+    name: "Cristian Duncan",
+    role: "Co-founder & COO",
+    quote: `“Distributed teams required unique processes. You need to approach work in a new way.”`,
+    twitter: "#",
+    linkedin: "#",
+  },
+  {
+    image: "/images/avatar-cruz.jpg",
+    name: "Cruz Hamer",
+    role: "Co-founder & CTO",
+    quote: `“Technology is at the forefront of enabling distributed teams. That's where we come in.”`,
+    twitter: "#",
+    linkedin: "#",
+  },
+  {
+    image: "/images/avatar-drake.jpg",
+    name: "Drake Heaton",
+    role: "Business Development Lead",
+    quote: `“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.”`,
+    twitter: "#",
+    linkedin: "#",
+  },
+  {
+    image: "/images/avatar-griffin.jpg",
+    name: "Griffin Wise",
+    role: "Lead Marketing",
+    quote: `“Unique perspectives shape unique products, which is what you need to survive these days.”`,
+    twitter: "#",
+    linkedin: "#",
+  },
+  {
+    image: "/images/avatar-aden.jpg",
+    name: "Aden Allan",
+    role: "Head of Talent",
+    quote: `“Empowered teams create truly amazing products. Set the north star and let them follow it.”`,
+    twitter: "#",
+    linkedin: "#",
+  },
+];
+
+export default directorsData;
