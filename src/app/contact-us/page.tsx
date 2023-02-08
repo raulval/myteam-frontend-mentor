175da63a23123ee6e876716eb27b96bc953115dd
Raul Val <raulval@outlook.com>
2023-02-08 15:46:38 -0300

    ✨ create contact us page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import patternTop from "@/assets/bg-patterns/bg-pattern-about-2-contact-1.svg";
+import patternBottom from "@/assets/bg-patterns/bg-pattern-contact-2.svg";
+import { Footer, Header, HeroContact } from "@/components";
+import {
+  Container,
+  Content,
+  Pattern1,
+  Pattern2,
+} from "@/styles/contact.styles";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content>
+          <HeroContact />
+          <Pattern1 src={patternTop} alt="pattern" />
+          <Pattern2 src={patternBottom} alt="pattern" />
+        </Content>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
